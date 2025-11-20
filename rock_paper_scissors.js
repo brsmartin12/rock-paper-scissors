0c9ef82400aa0dd95cb6d4a3b2aaffc8bdab8b35
brsmartin12 <44591396+brsmartin12@users.noreply.github.com>
2025-11-20 13:34:21 -0500

    Created computer and human choice functions.

---
+++ rock_paper_scissors.js
@@ -0,0 +1,29 @@
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+
+function getComputerChoice() {
+    const randomValue = getRandomInt(3);
+    if (randomValue === 0) {
+        console.log('computer selected rock');
+        return "rock";
+    } else if (randomValue === 1) {
+        console.log('computer selected paper');
+        return "paper";
+    } else {
+        console.log('computer selected scissors');
+        return "scissors";
+    }
+}
+
+
+function getHumanChoice() {
+    const humanInput = prompt("Rock, paper, or scissors?");
+    console.log("human selected" + " " + humanInput);
+    return humanInput;
+}
+
+
+getComputerChoice()
+getHumanChoice()
