51f9c2c39565ffb4967b718682fb17ec0ef50d6d
brsmartin12 <44591396+brsmartin12@users.noreply.github.com>
2025-11-20 16:18:28 -0500

    created the structure for the playRound function

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -22,8 +22,53 @@ function getComputerChoice() {
 function getHumanChoice() {
     const humanInput = prompt("Rock, paper, or scissors?");
     console.log("human selected" + " " + humanInput);
-    return humanInput;
+    humanInputStandardized = humanInput.toLowerCase();
+    console.log("human selection standardized to " + humanInputStandardized)
+    return humanInputStandardized;
 }
 
-getComputerChoice();
-getHumanChoice();
+function playRound() {
+    const computerChoice = getComputerChoice();
+    const humanChoice = getHumanChoice();
+
+    if (humanChoice === "rock") {
+        if (computerChoice === "paper") {
+            // computer wins
+            console.log(`You lose! ${computerChoice} beats ${humanChoice}.`)
+        } else if (computerChoice === "scissors") {
+            // human wins
+            console.log(`You win! ${humanChoice} beats ${computerChoice}`)
+        } else {
+            //tie
+            console.log(`It's a tie! ${humanChoice} and ${computerChoice}`)
+        }
+
+    } else if (humanChoice === "paper") {
+        if (computerChoice === "scissors") {
+            // computer wins
+            console.log(`You lose! ${computerChoice} beats ${humanChoice}.`)
+        } else if (computerChoice === "rock") {
+            // human wins
+            console.log(`You win! ${humanChoice} beats ${computerChoice}`)
+        } else {
+            //tie
+            console.log(`It's a tie! ${humanChoice} and ${computerChoice}`)
+        }
+
+    } else if (humanChoice === "scissors") {
+        if (computerChoice === "rock") {
+            // computer wins
+            console.log(`You lose! ${computerChoice} beats ${humanChoice}.`)
+        } else if (computerChoice === "paper") {
+            // human wins
+            console.log(`You win! ${humanChoice} beats ${computerChoice}`)
+        } else {
+            //tie
+            console.log(`It's a tie! ${humanChoice} and ${computerChoice}`)
+        }
+
+    } else {  // this is a typo
+        // Project constraints told us to assume correct input. I am just going to log a typo and do nothing else.
+        console.log('Oops! A typo.')
+    }
+}
